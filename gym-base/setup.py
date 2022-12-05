4989bf691bac910855f4f7c1f715e422a96cde31
Gilberto Briscoe-Martinez <gibr6485@colorado.edu>
2022-12-05 09:57:52 -0700

    added one more install requirment

--- gym-base/setup.py
+++ gym-base/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="gym_base",
     version="0.0.1",
-    install_requires=["gym==0.26.0", "pygame==2.1.0", "tqdm", "networkx", "numpy", "matplotlib"],
+    install_requires=["gym==0.26.0", "pygame==2.1.0", "tqdm", "networkx", "numpy", "matplotlib", "shapely"],
 )
